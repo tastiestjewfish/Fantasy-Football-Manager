d6d31885aa83babfa07383b6b45e3e2994ef7db8
tastiestjewfish <aaronmendelson@gmail.com>
2026-08-28 15:38:26 -0400

    Add API deps to root; sync lockfile for App Hosting

--- apphosting-server.js
+++ apphosting-server.js
@@ -5,7 +5,7 @@
  */
 const path = require("path");
 const express = require("express");
-const { app: apiApp } = require("./functions");
+const { app: apiApp } = require("./functions/app");
 
 const PORT = Number(process.env.PORT) || 8080;
 const HOST = "0.0.0.0";
